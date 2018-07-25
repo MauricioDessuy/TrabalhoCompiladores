a53671d726e9e907c12ffe8e1862fef9f439053e
MatthiasT1996 <matthi.trennepohl@gmail.com>
2018-07-24 21:33:05 -0300

    Lexico
    Criada classe Lexico

---
+++ src/shiftreduce/Lexico.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shiftreduce;
+
+/**
+ *
+ * @author matthias.trennepohl
+ */
+public class Lexico {
+    
+    public static String fonte = "";
+
+    public Lexico(String fonte) {
+        this.fonte = fonte;
+    }
+        
+    public static String reconheceNumero(){
+        String resultado = "";
+        int indice = 0;
+        int estado = 0;
+        
+        while (true) {
+            if (estado == 0) {
+                String s = fonte.substring(0, 1);
+                if(s.charAt(0) >= '0' && s.charAt(0) <='9'){
+                    estado = 1;
+                }
+                
+            }
+        }
+        
+    }
+    
+    public static String reconheceOperador(){
+        
+    }
+}
